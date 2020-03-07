f5cf8b2eca2102f455dcb77cb0f49ed94a60269c
ByungHoon-Ann <kjsx60@gmail.com>
2020-03-07 19:14:42 +0900

    Initial Commit

---
+++ PublicMyInterviewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PublicMyInterviewTest
+//
+//  Created by ByungHoon Ann on 2020/03/07.
+//  Copyright © 2020 ByungHoon Ann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
